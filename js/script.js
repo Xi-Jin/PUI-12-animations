ac1e5bc3fc6f6106c1860d331ec2c4f8d9708808
Alexandra To <alexandra.akiye.to@gmail.com>
2017-11-12 02:14:06 -0800

    Initial commit animations lab starter code

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//When .toggle() btn is clicked the div should appear and disappear
+$("#toggleFunc").on("click", function() {
+	$(".outer").toggle();
+});
+
+//When smooth .toggle() is clicked, the div should toggle slow 
+$("#toggleSmooth").on("click", function() {
+	//ADD CODE HERE
+});
+
+//When .slideUp() is clicked, the div shoudl swipe up to disappear slowly 
+$("#slidingUp").on("click", function() {
+	$(".outer").slideUp("slow");
+});
+
+//When .slideDown() btn is clicked, the div should swipe down to appear slowly 
+$("#slidingDown").on("click", function() {
+	//ADD CODE HERE
+});
+
+/* You can customize your own animation with .animate() */
+
+//When .slideUp() to height is clicked, the dis should swipe up to change height to 20px
+$("#slidingUpHeight").on("click", function() {
+	//ADD CODE HERE
+});
+
+//CREATE NEW FUNCTION HERE FOR .slideDown() to height click function 

2f45db87265c6e2589b3b8c3394893f6d019c530
Fannie Liu <fannie@cmu.edu>
2017-11-12 20:47:41 -0500

    adding animation chaining example

--- js/script.js
+++ js/script.js
@@ -3,26 +3,34 @@ $("#toggleFunc").on("click", function() {
 	$(".outer").toggle();
 });
 
-//When smooth .toggle() is clicked, the div should toggle slow 
+//When smooth .toggle() is clicked, the div should toggle slow
 $("#toggleSmooth").on("click", function() {
 	//ADD CODE HERE
 });
 
-//When .slideUp() is clicked, the div shoudl swipe up to disappear slowly 
+//When .slideUp() is clicked, the div should swipe up to disappear slowly
 $("#slidingUp").on("click", function() {
-	$(".outer").slideUp("slow");
+	//ADD CODE HERE
 });
 
-//When .slideDown() btn is clicked, the div should swipe down to appear slowly 
+//When .slideDown() btn is clicked, the div should swipe down to appear slowly
 $("#slidingDown").on("click", function() {
 	//ADD CODE HERE
 });
 
 /* You can customize your own animation with .animate() */
 
-//When .slideUp() to height is clicked, the dis should swipe up to change height to 20px
+//When .animate() slide up to height btn is clicked, the div should swipe up to change height to 20px
 $("#slidingUpHeight").on("click", function() {
 	//ADD CODE HERE
 });
 
-//CREATE NEW FUNCTION HERE FOR .slideDown() to height click function 
+//When .animate() slide down to height btn is clicked, the div should swipe down to change height to 300px
+$("#slidingDownHeight").on("click", function() {
+    //ADD CODE HERE
+});
+
+//When the .animate() slide up and down to height btn is clicked, the div should swipe up to change height to 20px and then back down to change height to 50px
+$("#slidingUpDownHeight").on("click", function() {
+    //ADD CODE HERE
+});
